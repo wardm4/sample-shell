ffb877ed795a75c06ee9befc74943c1c86e158fa
matt <NSquarticsurface@gmail.com>
2014-04-15 15:08:08 -0700

    adds input

--- shell.c
+++ shell.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define MAX_LINE 80
 
-void setup(char inputBuffer[], char *args[], int *background) {
-  /** fill in later */
-}
-
 int main(void) {
-  char inputBuffer[MAX_LINE];
-  int background;
   char *args[MAX_LINE/2 +1];
+  int should_run = 1;
+
+  int i, upper;
   
-  while (1) {
-    background = 0;
-    printf(" sh ->");
-    setup(inputBuffer, args, &background);
+  while (should_run) {
+    printf("sh>");
+    fflush(stdout);
+    scanf("%s", *args);
+    
+
+    //Fork a child process
+
+    int pid = fork();
+    if (pid != 0) {
+      wait(NULL); //Waits for child process to terminate
+    }
+    else {
+      execvp(args[0], args);
+    }
 
     /** Fill in later */
   }
